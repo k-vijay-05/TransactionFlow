63f4f00a077f97bd8763e4b1a242c5c64d94710a
vijay <kvijay20204567@gmail.com>
2024-10-18 00:43:07 +0530

    userschema done

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require('dotenv').config();
+const app=express()
+app.use(express.json())
+const connectDb = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("mongodb connected succesfully");
+    }
+    catch (err) {
+        console.log(err);
+    }
 
-
+};
+connectDb();
+app.listen(3001, () => {
+    console.log("server is running");
+})
